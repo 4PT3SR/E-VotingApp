79e2ef8489b947858e6ad6bc9eb7fb869210bca6
willdera <godswillezeoke@gmail.com>
2023-06-20 17:54:09 +0100

    get votes

--- E-Voting/src/EVoting.sol
+++ E-Voting/src/EVoting.sol
@@ -8,12 +8,18 @@ contract EVoting is Ownable {
     using Counters for Counters.Counter;
     using Counters for uint256;
     Counters.Counter private electionId;
+    Counters.Counter private candidateId;
 
     uint256 public number;
 
+    // user hash => bool: true = whitelisted, false = not whitelisted (can't vote)
     mapping (bytes32 => bool) public isWhitelisted;
+    // electionId => ElectionInfo
     mapping (uint256 => Election) public Elections;
-    mapping (uint256 => Candidate) public CandidateStats;
+    // electionId => candidateId index => Candidate Info
+    mapping (uint256 => Candidate[]) public CandidateStats;
+    // electionId => voter hash => bool: true = voter has casted a vote, false voter has not casted a vote
+    mapping (uint256 => mapping (bytes32 => bool)) private userVoted;
 
     enum ElectionType {
       College,
@@ -43,11 +49,8 @@ contract EVoting is Ownable {
     event ElectionCreated(string indexed _title, ElectionType indexed _electionType);
     event VoterRegistered(string indexed _matNo);
     event CandidateCreated(string indexed _candidateName, ElectionType indexed _electionType, uint256 indexed _electionId);
+    event VoteCasted(uint256 indexed _electionId, uint256 indexed _candidateId);
     
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
     function createElectiton(string memory _title, uint256 _start, uint256 _end, uint8 _electionType) public onlyOwner {
       require(block.timestamp <= _start && _start < _end, "EVoting: Invalid election duration!");
       require(_electionType < 3, "EVoting: Election type!");
@@ -75,7 +78,7 @@ contract EVoting is Ownable {
         election_type: ElectionType(_electionType)
       });
 
-       CandidateStats[_electionId] = _candidate;
+       CandidateStats[_electionId].push(_candidate);
 
        emit CandidateCreated(_candidateName, ElectionType(_electionType), _electionId);
     }
@@ -87,7 +90,16 @@ contract EVoting is Ownable {
       emit VoterRegistered(_matNo);
     }
 
-    // function castVote(uint256 _electionId, uint256 _candidateId, string memory _matNo, string memory _email) public onlyOwner isWhitelistedToVote(_matNo, _email) {
-    //   require
-    // }
+    function castVote(uint256 _electionId, uint256 _candidateId, string memory _candidateName, string memory _matNo, string memory _email) public onlyOwner isWhitelistedToVote(_matNo, _email) {
+      Candidate memory _candidateTemp = CandidateStats[_electionId][_candidateId - 1];
+      require(keccak256(abi.encodePacked(_candidateTemp.candidateName)) == keccak256(abi.encodePacked(_candidateName)), "EVoting: Candidate mismatch!");
+      
+      CandidateStats[_electionId][_candidateId - 1].voteCount++;
+
+      emit VoteCasted(_electionId, _candidateId - 1);
+    }
+
+    function getVotesByElection(uint256 _electionId) public view returns(Candidate[] memory) {
+      return CandidateStats[_electionId];
+    }
 }
