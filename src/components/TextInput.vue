97b68e70b65ccc789844b95b2fd08d32f0cf9bb4
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-21 10:32:39 +0100

    feat: create input component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface InputProps {
+    modelValue: string | number
+    error?: string
+    label: string
+}
+
+defineProps<InputProps>()
+
+const baseInputEmits = defineEmits<{
+    (e: 'update:modelValue', value: string | number): void
+}>()
+
+const handleInputChange = (event: Event) => (event.target as HTMLInputElement).value
+</script>
+
+<template>
+    <div class="border-none p-0 relative grid gap-1">
+        <label class="e-label">{{ label }}</label>
+        <input v-bind="$attrs" :value="modelValue" @change="baseInputEmits('update:modelValue', handleInputChange($event))" class="e-input" />
+    </div>
+</template>
+
+<style scoped>
+@import url("~/form.css");
+.e-input {
+    @apply w-full border-gray-300;
+}
+
+.e-label {
+    @apply text-sm text-gray-700 font-medium;
+}
+</style>
