8712d753947137dda1921e313b9f52e1a1fa3582
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-23 21:01:09 +0100

    fix: cors

--- src/routes/testRoute.js
+++ src/routes/testRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-    res.send('you have reached the test route');
+    res.send('you may have reached the test route');
 });
 
 module.exports = router;
