ef846f556bde5f12028921aa28480b74af03eb67
apt3sr <kingtoluene@gmail.com>
2023-06-23 14:30:03 +0100

    aded @cors localhost

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 // cors config
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
