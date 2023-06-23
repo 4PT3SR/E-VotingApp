50eb06358adee810fe3d20c40199fc855db9686c
apt3sr <kingtoluene@gmail.com>
2023-06-23 14:49:47 +0100

    test cors with credential

--- src/index.js
+++ src/index.js
@@ -29,9 +29,13 @@ const electionRouter = require('./routes/electionRoute')
 
 const app = express();
 const PORT = process.env.PORT || 6900;
-app.use(cors({
-    origin: ['http://*', 'https://*']
-}));
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(limiter)
