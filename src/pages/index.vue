38e5fbd53bdb8e18c8c8816fbc686ea20dff0772
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-26 11:25:55 +0100

    feat: fetch election

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import { useBaseFetch } from '~/composables/fetch';
 
+const {
+    get,
+    data,
+    error,
+    onFetchResponse,
+    onFetchError
+} = useBaseFetch<string>('/election', { immediate: false }).json()
+
+onFetchResponse(async () => {
+  console.log(data.value)
+})
+
+onFetchError(async () => {
+  console.log(error.value)
+})
+
+get().execute()
 </script>
 
 <template>
     <div class="grid grid-cols-2 h-full">
-
+      
     </div>
 </template>
 
