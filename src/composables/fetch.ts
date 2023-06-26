2b657d5710d286eec93f3a659a068c1c99be7c9b
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-26 11:25:40 +0100

    feat: add authToken to http instance

--- src/composables/fetch.ts
+++ src/composables/fetch.ts
@@ -1,9 +1,19 @@
 import { createFetch } from '@vueuse/core'
+import { userStore } from '~/store/user'
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const useBaseFetch = createFetch({
   baseUrl: BASE_URL,
+  options: {
+    // beforeFetch in pre-configured instance will only run when the newly spawned instance do not pass beforeFetch
+    async beforeFetch({ options }) {
+      const user = userStore()
+      const myToken = user.token
+      options.headers!.Authorization = `Bearer ${myToken}`
+      return { options }
+    },
+  },
   fetchOptions: {
     headers: {
       Accept: 'application/json',
