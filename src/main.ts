dc621f9e97c5c8a7f49c80ac1456f87f4638d78c
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-26 11:25:10 +0100

    feat: use pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import { router } from './router'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+pinia.use(({ store }) => {
+    store.$router = markRaw(router)
+})
+app.use(pinia)
 app.use(router)
 app.mount('#app')
