84ef473757cedc4db3cced9c0439302e57b93120
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-27 10:46:08 +0100

    feat: create Election type

---
+++ src/types/election.ts
@@ -0,0 +1,12 @@
+export interface Election {
+    _id: string
+    title: string
+    election_type: string
+    posts: string[]
+    start: string
+    end: string
+    department_eligibility: string
+    createdAt: string
+    updatedAt: string
+    __v: number
+}
