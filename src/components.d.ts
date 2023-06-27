d41e6b4b4c0fcbd8a8e220d670969c7ece3a0a26
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-06-27 11:44:59 +0100

    fix: other fixes

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('./components/Button.vue')['default']
     Header: typeof import('./components/Header.vue')['default']
+    Loader: typeof import('./components/Loader.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TextInput: typeof import('./components/TextInput.vue')['default']
