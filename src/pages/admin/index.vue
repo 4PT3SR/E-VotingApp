a2e6fd15edc0c82c5443973d3c9c443a67ba91e7
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-07-10 14:25:57 +0100

    feat: add create user button

--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { useAxiosInstance } from '~/composables/useAxiosInstance';
 import type { User } from "~/types/user";
+import { userStore } from '~/store/user';
 
 const api = useAxiosInstance()
+const store = userStore()
 const isFetching = ref(false)
 const users = ref<User[]>([])
 
@@ -45,8 +47,9 @@ onMounted(async() => {
         <div class="max-w-screen-xl px-5 lg:px-0 mx-auto h-full">
             <Loader v-if="isFetching" />
             <div v-else class="border border-[#EAECF0] rounded-lg shadow-none lg:shadow-md">
-                <div class="px-6 py-4 border-b border-b-[#EAECF0]">
+                <div class="flex w-full items-center justify-between px-6 py-4 border-b border-b-[#EAECF0]">
                     <h1 class="font-medium text-lg text-gray-900">Users</h1>
+                    <Button label="Add user" rounded size="small" />
                 </div>
                 <div class="lg:w-full lg:left-auto lg:relative lg:right-auto lg:overflow-x-hidden left-0 right-0 overflow-x-scroll">
                     <table class="table-auto w-full">
@@ -70,7 +73,7 @@ onMounted(async() => {
                                 <td class="px-6 py-4">
                                     <div class="flex items-center justify-end gap-3">
                                         <button v-if="!user.isAdmin" type="button" class="text-sm font-semibold text-green-700" @click="activateUser(user._id)">Activate</button>
-                                        <button v-else type="button" class="text-sm font-semibold text-red-700" @click="deactivateUser(user._id)">Deactivate</button>
+                                        <button v-else type="button" class="text-sm font-semibold text-red-700 disabled:cursor-not-allowed" :disabled="user._id === store.user?._id" @click="deactivateUser(user._id)">Deactivate</button>
                                     </div>
                                 </td>
                             </tr>
