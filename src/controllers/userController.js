5fca7997032eb105e8df94ecc24f7f2618a86913
apt3sr <kingtoluene@gmail.com>
2023-07-17 00:24:08 +0100

    updated puppeteer args and executables for deployment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,15 @@ const contract = require("../helpers/contract_connect");
 exports.register = async (req, res, next) => {
   try {
     const body = await registerSchema.validateAsync(req.body);
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
+    });
 
     const page = await browser.newPage();
     await page.goto('https://portal.bellsuniversity.edu.ng/');
